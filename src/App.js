7b93b84c525d880e77e6fd82b89b8c5fb97ebc77
Inokcode <regilearn@gmail.com>
2022-06-02 16:14:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <input type="text" placeholder="Search..." className="search" />
+    </div>
+  );
+}
+
+export default App;
